554fd9944ba87c3b1c4235be9beb3ffb445f7195
mwenski <mwenski@outlook.com>
2021-08-31 19:55:55 +0200

    Still working with simulator

--- src/main/java/com/example/algorytmy_kolekcjonerskiej_wymiany_wzajemnej/Symulator.java
+++ src/main/java/com/example/algorytmy_kolekcjonerskiej_wymiany_wzajemnej/Symulator.java
@@ -1,11 +1,12 @@
 package com.example.algorytmy_kolekcjonerskiej_wymiany_wzajemnej;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 
@@ -19,33 +20,150 @@ public class Symulator extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Symulator obliczeniowy");
 
-        GridPane inputTable = new GridPane();
-        GridPane dataTable = new GridPane();
+        BorderPane borderPane = new BorderPane();
+        HBox topHBox = topHBox();
+        VBox leftVBox = leftVBox();
+        VBox rightVBox = rightVBox();
+        HBox bottomHBox = bottomHBox();
+        borderPane.setTop(topHBox);
+        borderPane.setLeft(leftVBox);
+        borderPane.setRight(rightVBox);
+        borderPane.setBottom(bottomHBox);
 
-        for (int i = 0; i < 2; i++){
-            for (int j = 0; j < 3; j++){
-                TextField inputTableCell = new TextField();
-                inputTable.add(inputTableCell, j, i);
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public HBox topHBox(){
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(15, 12, 15, 12));
+        hBox.setSpacing(10);
+
+        Button buttonNew = new Button("Nowy");
+        buttonNew.setPrefSize(100, 20);
+
+        Button buttonReport = new Button("Wygeneruj raport");
+        buttonReport.setPrefSize(200, 20);
+
+        hBox.getChildren().addAll(buttonNew, buttonReport);
+
+        return hBox;
+    }
+
+    public VBox leftVBox(){
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(15, 12, 15, 12));
+        vBox.setSpacing(10);
+
+        GridPane dataTable = new GridPane();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i == 0 && j==0){
+                    Label label = new Label("Premia: ");
+                    dataTable.add(label, 0, 0);
+                }else if(i == 0){
+                    Label label = new Label("Cena przedmiotu #" + String.format("%d", j-1) + ": ");
+                    dataTable.add(label, 0, j);
+                }else {
+                    TextField inputDataTableCell = new TextField();
+                    inputDataTableCell.setPrefSize(40, 20);
+                    dataTable.add(inputDataTableCell, i, j);
+                }
             }
         }
 
-        for (int j = 0; j < 3; j++){
-            TextField inputDataTableCell = new TextField();
-            dataTable.add(inputDataTableCell, 0, j);
+        //TODO: Sprawdzić co jest nie tak z gridem
+        GridPane inputTable = new GridPane();
+        //inputTable.setMinSize(400,400);
+        //inputTable.setMaxSize(400,400);
+
+        int rowCount = 4;
+        int columnCount = 5;
+
+        RowConstraints rc = new RowConstraints();
+        rc.setPercentHeight(100d / rowCount);
+
+        for (int i = 0; i < rowCount; i++) {
+            inputTable.getRowConstraints().add(rc);
         }
-        /*
-        Button button = new Button("Click to get text");
 
-        button.setOnAction(action -> {
-            System.out.println(textField.getText());
-        });
+        ColumnConstraints cc = new ColumnConstraints();
+        cc.setPercentWidth(100d / columnCount);
 
-        HBox hbox = new HBox(textField, button);
-         */
+        for (int i = 0; i < columnCount; i++) {
+            inputTable.getColumnConstraints().add(cc);
+        }
 
-        Scene scene = new Scene(inputTable, 240, 100);
-        //TODO: Pokombinować z tymi gridami
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Label label = new Label("   ");
+        inputTable.add(label, 0, 0, 2, 2);
+        Label labelWidth = new Label("Ilość przedmiotów");
+        inputTable.add(labelWidth, 2, 0, 3, 1);
+        Label labelHeight = new Label("Użytkownik");
+        inputTable.add(labelHeight, 0, 2, 1, 2);
+        for (int i = 0; i < 3; i++){
+            Label label1 = new Label("#" + String.format("%d", i));
+            inputTable.add(label1, (2+i), 1, 1, 1);
+        }
+        for (int i = 0; i < 2; i++){
+            Label label2 = new Label("#" + String.format("%d", i));
+            inputTable.add(label2, 1, (2+i), 1, 1);
+        }
+
+        for (int i = 3; i < 5; i++){
+            for (int j = 3; j < 6; j++){
+                TextField inputTableCell = new TextField();
+                //inputTableCell.setPrefSize(40, 20);
+                inputTable.add(inputTableCell, j, i, 1, 1);
+            }
+        }
+
+        vBox.getChildren().addAll(dataTable, inputTable);
+
+        return vBox;
+    }
+
+    public VBox rightVBox(){
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(15, 12, 15, 12));
+        vBox.setSpacing(10);
+
+        GridPane firstTable = new GridPane();
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 3; j++){
+                TextField inputFirstCell = new TextField();
+                inputFirstCell.setPrefSize(40, 20);
+                firstTable.add(inputFirstCell, j, i);
+            }
+        }
+
+        GridPane lastTable = new GridPane();
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 3; j++){
+                TextField inputLastCell = new TextField();
+                inputLastCell.setPrefSize(40, 20);
+                lastTable.add(inputLastCell, j, i);
+            }
+        }
+
+        vBox.getChildren().addAll(firstTable, lastTable);
+
+        return vBox;
+    }
+
+    public HBox bottomHBox(){
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(15, 12, 15, 12));
+        hBox.setSpacing(10);
+
+        Button buttonNew = new Button("Start");
+        buttonNew.setPrefSize(100, 20);
+
+        Button buttonReport = new Button("Wygeneruj raport");
+        buttonReport.setPrefSize(200, 20);
+
+        hBox.getChildren().addAll(buttonNew, buttonReport);
+
+        return hBox;
     }
 }
