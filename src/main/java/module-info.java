515eb648797b9177828a570cf61ae4689ab8af64
mwenski <mwenski@outlook.com>
2021-11-11 19:04:10 +0100

    Almost

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,10 +6,10 @@ module com.example.algorytmy_kolekcjonerskiej_wymiany_wzajemnej {
     requires poi;
     requires poi.ooxml.schemas;
 
-    opens com.example.Simulator to javafx.fxml;
     exports com.example.Simulator;
     exports com.example.ReadWriteFile;
-    opens com.example.ReadWriteFile to javafx.fxml;
     exports com.example.Algorithms;
+    opens com.example.Simulator to javafx.fxml;
+    opens com.example.ReadWriteFile to javafx.fxml;
     opens com.example.Algorithms to javafx.fxml;
 }
